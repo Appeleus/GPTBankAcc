761dd8fc983cdfebca963412a624d929c2f25180
Delicious_Apple <110074066+Appeleus@users.noreply.github.com>
2023-08-03 17:37:35 +0700

    Update Deposit / Withdraw and more.
    - Cleaning up Main method.
    - Scanner added to wait for input.
    - Updated Deposit and Withdraw with input from scanner.
    - Check Balance might get hard after the implementation of local JSON database (not on this update as of now)

--- JavaBankSys.java
+++ JavaBankSys.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 
 public class JavaBankSys implements Runnable {
 
@@ -15,17 +16,20 @@ public class JavaBankSys implements Runnable {
         String ThreadName = Thread.currentThread().getName();
         System.out.println(ThreadName + " reporting for duty!");
         // Parameters are being send from constructor's help.
-        if (select.equals("D")) {
+        try {
+        if (select.equals("2")) {
             System.out.println("---------------\nDeposit activated");
             Deposit(amount);
-            Deposit(10000);
-            Deposit(5000);
-        } else if (select.equals("W") && !(balance <= 0)) {
+            System.out.println("! Deposit Successful !");
+        } else if (select.equals("3") && !(balance <= 0)) {
             System.out.println("---------------\nWithdraw activated");
             Withdraw(amount);
-            Withdraw(999999999);
+            System.out.println("! Withdrawal Successful !");
         } else {
-            System.out.println("-------Transaction failed, please try again.-------");
+            System.out.println("------- Transaction failed, please try again.-------");
+        }
+        } catch (Exception e) {
+            System.out.println("Please enter in the correct format <number>");
         }
 
     }
@@ -55,11 +59,79 @@ public class JavaBankSys implements Runnable {
     }
 
     public void CheckBalance() {
-        System.out.println("Your current balance : " + balance);
+        System.out.println("Your current balance : $" + balance);
     }
 
     public void AccountList() {
         System.out.println("This system will be implement later on");
+    }
+
+    public static void WaitUserChoice(Scanner userChoice) {
+        System.out.println("Welcome to the Simple Banking System!\r\n" + 
+                "1. Create a new bank account\r\n" + //
+                "2. Deposit money\r\n" + //
+                "3. Withdraw money\r\n" + //
+                "4. Check balance\r\n" + //
+                "5. Display all accounts\r\n" + //
+                "6. Exit\r\n");
+        System.out.print("Enter your choice: ");
+        String userChoiceSelected = userChoice.nextLine();
+        System.out.println("------ You have chosen : " + userChoiceSelected + " ------");
+
+        switch(userChoiceSelected) {
+            case "1":
+                break;
+            case "2":
+            case "3":
+                String userSelection = String.valueOf(userChoiceSelected);
+                Scanner userMoneyAddInitialize = new Scanner(System.in);
+                String msg;
+                try {
+
+                if (userSelection.equals("2")) {
+                        msg = "deposit";
+                        System.out.print("Enter Amount to " + msg + " : ");
+                    } else if (userSelection.equals("3")) {
+                        msg = "withdraw";
+                        System.out.print("Enter Amount to " + msg + " : ");
+                    }
+                int userMoneyAmount = userMoneyAddInitialize.nextInt();
+                // Create 2 Threads
+                JavaBankSys j1 = new JavaBankSys(userSelection, userMoneyAmount);
+                JavaBankSys j2 = new JavaBankSys(userSelection, userMoneyAmount);
+
+                Thread t1 = new Thread(j1);
+                Thread t2 = new Thread(j2);
+
+                    if (userSelection.equals("2")) {
+                        t1.start();
+                    } else if (userSelection.equals("3")) {
+                        t2.start();
+                    }
+                    t1.join();
+                    t2.join();
+                } catch (Exception e) {
+                    System.out.println("You've made a mistake, please try again.");
+                }
+                break;
+            case "4":
+                JavaBankSys chkBalance = new JavaBankSys();
+                chkBalance.CheckBalance();
+                break;
+            case "5":
+                break;
+            case "6":
+                System.out.println("Thank you for using the Simple banking System. Goodbye!");
+                System.exit(1);
+                break;
+            default:
+                System.out.println("Please select the choices above. <number>");
+            break;
+        }
+    }
+
+    public JavaBankSys() {
+        System.out.println("> Empty JavaBankSys constructor has been called.");
     }
 
     // Constructor helps in initalizing and sending paremeters to runnable's run()
@@ -67,7 +139,7 @@ public class JavaBankSys implements Runnable {
     public JavaBankSys(String select, int amount) {
         this.select = select;
         this.amount = amount;
-        System.out.println("JavaBankSys constructor has been called.");
+        System.out.println("> JavaBankSys constructor has been called.");
     }
 
 }
@@ -75,32 +147,9 @@ public class JavaBankSys implements Runnable {
 class Main {
     public static void main(String[] args) {
 
-        if (args.length != 2) {
-            System.out.println("Usage : <(D)eposit / (W)ithdraw> <Amount>");
-            System.exit(1);
-        } else {
-            System.out.println("the initialization is complete!\n---------------");
+        // START OF PROGRAM, WAIT FOR USER SELECTION
+        Scanner scanner = new Scanner(System.in);
+        JavaBankSys.WaitUserChoice(scanner);
 
-            String userSelection = args[0];
-            int userMoneyAmount = Integer.parseInt(args[1]);
-            // Create 2 Threads
-            JavaBankSys j1 = new JavaBankSys(userSelection, userMoneyAmount);
-            JavaBankSys j2 = new JavaBankSys(userSelection, userMoneyAmount);
-
-            Thread t1 = new Thread(j1);
-            Thread t2 = new Thread(j2);
-
-            try {
-                if (userSelection.equals("D")) {
-                    t1.start();
-                } else if (userSelection.equals("W")) {
-                    t2.start();
-                }
-                t1.join();
-                t2.join();
-            } catch (Exception e) {
-                System.out.println("you've made some mistake, please try again");
-            }
-        }
     }
 }
